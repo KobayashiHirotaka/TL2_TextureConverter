d4987502d4076144bac95f8268e303a8e1e628a1
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-10-07 15:31:32 +0900

    資料は全部書き終わった

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -12,5 +12,26 @@ TextureConverter::~TextureConverter()
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
+	LoadWICTextureFromFile(filePath);
+}
 
+void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
+{
+	//ファイルパスをワイド文字列に変換する
+	std::wstring wFilePath = ConvertMultiByteStringToWideString(filePath);
+}
+
+std::wstring ConvertMultiByteStringToWideString(const std::string& mString)
+{
+	//ワイド文字列に変換した際の文字数を計算
+	int filePathBufferSize = MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
+
+	//ワイド文字列
+	std::wstring wString;
+	wString.resize(filePathBufferSize);
+
+	//ワイド文字列に変換
+	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], filePathBufferSize);
+
+	return wString;
 }
--- TextureConverter.h
+++ TextureConverter.h
@@ -1,16 +1,24 @@
 #pragma once
 #include <string>
+#include <stringapiset.h>
 
 class TextureConverter
 {
 public:
+	//コンストラクタ
 	TextureConverter();
 
+	//デストラクタ
 	~TextureConverter();
 
+	//テクスチャをWICからDDSに変換する
 	void ConvertTextureWICToDDS(const std::string& filePath);
 
 private:
+	//テクスチャファイル読み込み
+	void LoadWICTextureFromFile(const std::string& filePath);
 
+	//マルチバイト文字列をワイド文字列に変換
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
 };
 
